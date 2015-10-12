89a4e93eedf4c375eaf49ba277977a6896b2dcda
akil-innov8 <akil@innov8labs.in>
2015-10-12 18:23:40 +0530

    Parse Basic

---
+++ Assets/_Scripts/CreateWord.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateWord : MonoBehaviour {
+	public string word;
+	public GameObject WordGrid,LetterGrid;
+	// Use this for initialization
+	void Start () {
+		//Invoke("Create",1);
+		word = "Doctor";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Create(){
+
+	}
+}
---
+++ Assets/_Scripts/ParseManager.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Parse;
+
+public class ParseManager : MonoBehaviour {
+	public List<string> Words,Easy,Medium,Hard;
+	// Use this for initialization
+	void Start () {
+//		ParseObject testObject = new ParseObject("UnityObject");
+//		testObject["UserName"] = "Akil";
+//		testObject.SaveAsync();
+
+
+
+		//StartCoroutine("RetrieveData");
+	
+		GetEasyWords();
+
+		//GetWords();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator RetrieveData(){
+
+
+
+		var query = ParseObject.GetQuery("UnityObject").WhereExists("UserName");
+		query.FindAsync().ContinueWith(t =>
+		                               {
+			IEnumerable<ParseObject> results = t.Result;
+			foreach (var result in results) {
+				Debug.Log("Name: " + result["UserName"]);
+			}
+		});
+
+
+		yield return null;
+
+	}
+
+
+	void GetEasyWords(){
+		var query = ParseObject.GetQuery("NewDictionary").WhereExists("type");
+		query.FindAsync().ContinueWith(t =>
+		                               {
+			IEnumerable<ParseObject> results = t.Result;
+			foreach (var result in results) {
+				Words.Add(result["Word"].ToString());
+
+				if(result["type"].ToString()=="E")
+					Easy.Add(result["Word"].ToString());
+			}
+		});
+	}
+
+	void GetWords(){
+		var query = ParseObject.GetQuery("Dictionary").WhereExists("Level");
+		query.FindAsync().ContinueWith(t =>
+		                               {
+			IEnumerable<ParseObject> results = t.Result;
+			foreach (var result in results) {
+				
+				if(result["Level"].ToString()=="23")
+					Words.Add(result["Word"].ToString());
+			}
+		});
+	}
+}
